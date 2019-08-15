4ed5062b9d7b72d03cd0547a3f5428a37ca0ce9a
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-15 21:38:07 +0300

    models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,50 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_15_181624) do
+ActiveRecord::Schema.define(version: 2019_08_15_183542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.float "longitude"
+    t.float "latitude"
+    t.bigint "user_id"
+    t.integer "zip"
+    t.string "city"
+    t.string "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
+  end
+
+  create_table "donations", force: :cascade do |t|
+    t.bigint "user_id"
+    t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_donations_on_user_id"
+  end
+
+  create_table "soldiers_donations", force: :cascade do |t|
+    t.bigint "donation_id"
+    t.bigint "user_id"
+    t.float "amount_per_soldier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["donation_id"], name: "index_soldiers_donations_on_donation_id"
+    t.index ["user_id"], name: "index_soldiers_donations_on_user_id"
+  end
+
+  create_table "tax_receipts", force: :cascade do |t|
+    t.integer "number"
+    t.bigint "donation_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["donation_id"], name: "index_tax_receipts_on_donation_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,8 +63,16 @@ ActiveRecord::Schema.define(version: 2019_08_15_181624) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "role"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "addresses", "users"
+  add_foreign_key "donations", "users"
+  add_foreign_key "soldiers_donations", "donations"
+  add_foreign_key "soldiers_donations", "users"
+  add_foreign_key "tax_receipts", "donations"
 end
