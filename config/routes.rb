fce610544a93d04bde0aa4a9dd00cf27f3519094
Bryan Leighton <32174912+bdlb77@users.noreply.github.com>
2019-08-16 00:44:40 +0300

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -15,13 +15,6 @@ Rails.application.routes.draw do
     resources :users do
       member do
         get 'dashboard'
-      end
-    end
-  end
-
-  namespace :donors do
-    resources :users do
-      member do
         get 'receipts'
       end
     end
