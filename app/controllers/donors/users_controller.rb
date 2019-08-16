47cd7a45deacead2043556ce95aebf219d5ffba8
yosupgurl <benabitbul@hotmail.com>
2019-08-15 21:05:44 -0400

    removed someones binding.pry

--- app/controllers/donors/users_controller.rb
+++ app/controllers/donors/users_controller.rb
@@ -3,10 +3,8 @@ class Donors::UsersController < ApplicationController
 	def dashboard
 		@receipts  = TaxReceipt.all
 		@donations_count= current_user.donations.count
-
 		soldiers = current_user.donations.map(&:donated_soldiers)
 		@total_soldiers = soldiers&.flatten.uniq.count
-		binding.pry
 	end
 
 	def receipts
