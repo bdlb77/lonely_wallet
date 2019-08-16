908677b04e62cee57611856fef7b948490c5aafc
Bryan Leighton <bryan.leigh77@gmail.com>
2019-08-16 03:43:44 +0300

    dashboard

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,5 @@
-import "bootstrap";
+import 'bootstrap';
+import { tabToggle } from './modules/tab';
+
+const donorDashOverview = document.querySelector('#donor_dash_overview');
+if (donorDashOverview) tabToggle();
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,4 +2,5 @@ class Donation < ApplicationRecord
   belongs_to :user
   has_many :tax_receipts
   has_many :soldiers_donations
+  has_many :donated_soldiers, through: :soldiers_donations, source: :user
 end
